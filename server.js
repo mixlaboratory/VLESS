92ff198270e4ef3fe754e8665a35f8dbfd2b8e8a
chimamedia <SHO.OOI@outlook.jp>
2025-01-20 22:43:06 +0900

    1/20　大井　修正

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 const io = require('socket.io')(server);
 
 // 静的ファイルを提供
-app.use(express.static( __dirname + "/public"));
+app.use(express.static( __dirname + "/docs"));
 
 // 動画の状態管理
 let videoState = {
